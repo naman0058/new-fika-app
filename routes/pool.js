e472e86851d5579fd3ffb168c27f2f7f2835296c
filemakrxpert <filemakrxpert@gmail.com>
2025-08-06 17:20:49 +0530

    fix

--- routes/pool.js
+++ routes/pool.js
@@ -1,17 +1,14 @@
-
-var mysql = require('mysql')
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
- 
-  host : 'db-mysql-blr1-78922-do-user-4199968-0.c.db.ondigitalocean.com',
-   user: 'doadmin',
-  password:'AVNS_GQrps4okJNd-Q3VSn68',
-    database: 'leffet',
-    port:'25060',
-    multipleStatements: true,
-    charset: 'utf8mb4'  // << VERY IMPORTANT
-  })
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  multipleStatements: true,
+  charset: 'utf8mb4'
+});
 
 module.exports = pool;
